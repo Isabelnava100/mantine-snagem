f2d1844ee6828a2e8c3ee551a307e5b08b7ed1c4
Isabelnava100 <isabelnava100@gmail.com>
2022-05-30 17:56:53 -0400

    error fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes as Switch, Route } from 'react-router-dom';
+import { MantineProvider } from '@mantine/core';
+import './assets/styles/index.css';
+
+import Home from './pages/HomePage';
+import SharedLayout from './pages/SharedLayout';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={{
+      colorScheme:'dark',
+      primaryShade:8,
+      primaryColor:'grape'
+    }} withGlobalStyles withNormalizeCSS>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" element={<SharedLayout/>}>
+            <Route index element={<Home/>}/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+
+    </MantineProvider>
+    
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+
+
+function Home() {
+  return (
+    <div className="App">
+     test
+    </div>
+  );
+}
+
+export default Home;
