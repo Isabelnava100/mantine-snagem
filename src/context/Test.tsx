27676521d1fcfbbfc7107c75a80d5913e0f6a31f
Isabelnava100 <isabelnava100@gmail.com>
2022-06-07 12:02:23 -0400

    context

---
+++ src/context/Test.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+
+export const Test = () => {
+
+
+  //   const [forumname, setForumname] = useState("");
+  //   const getForums = async()=>{
+  //   await axios
+  //   .post(`${url}forums/forums.php`, {
+  //     forumParam: id,
+  //   })
+  //   .then((result) => {
+  //     console.log(result.data);
+    
+  //   }).catch(err => console.log(err));;
+  // }
+  
+  //   useEffect(async () => {
+  //     getForums();
+  //   }, []);
+
+  return (
+    <div>Test</div>
+  )
+}
